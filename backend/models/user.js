bde5ab1bdd922e029700017eabfacd76b9321896
Vikky Verma <Vikkyverma7054@gmail.com>
2026-06-04 12:29:16 +0530

    Fix mangoose pre-save hook

--- backend/models/user.js
+++ backend/models/user.js
@@ -23,10 +23,10 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // ✅ Hash password before saving
-userSchema.pre("save", async function(next) {
-    if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) return;
+
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 const User = mongoose.model("User", userSchema);
